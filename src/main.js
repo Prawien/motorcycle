f67eb0e95eda0c66331968ec737a2d5eb10e0b1f
Prawien <prawien.baktawar@dpdk.com>
2017-11-17 20:42:19 +0100

    Added all the things to a class and divided the wheels in 6 pieces.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 //Canvas
 let canvas = document.getElementById('canvas');
 let ctx;
+const TWO_PI = Math.PI * 2;
 
-//Drawing variables
-let curX = 0;
-let curY = 0;
-
+//Init
 function initCanvas(){
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
@@ -14,80 +12,146 @@ function initCanvas(){
   middleY = canvas.height / 2;
   ctx = canvas.getContext('2d');
 
-  drawMotorcycle();
+  new Motorcycle(middleX, middleY);
 }
 
-function drawMotorcycle() {
-  drawHull();
-  drawFrontWheel();
+//Classes
+class Motorcycle {
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+
+    this.hullLines = [
+      //Wheelcover(back)
+      [175, 0],
+      [0, -25],
+      [75, -50],
+      [-15, -5],
+      [-75, 15],
+      //Tail
+      [5, -20],
+      [125, -50],
+      [-125, -5],
+      [-10, 25],
+      //Seat
+      [-70, 5],
+      [-30, -30],
+      [-70, 5],
+      [-25, 15],
+      //Front
+      [-10, 0],
+      [25, -50],
+      [-20, 5],
+      [-75, 100],
+      //Wheelcover(front)
+      [50, 0],
+      [70, 65],
+    ];
+    this.wheelcon1 = [
+      [-50, 45],
+      [-5, -5],
+      [50, -45]
+    ]
+
+    this.draw();
+  }
+
+  draw(){
+    // Hull
+    ctx.beginPath();
+    for(let i = 0;i < this.hullLines.length;i++){
+      this.line(this.hullLines[i][0], this.hullLines[i][1]);
+    }
+    ctx.closePath();
+    ctx.fillStyle = 'purple';
+    ctx.fill();
+    ctx.stroke();
+
+    //Connection to Hull 1
+    ctx.beginPath();
+    this.move(-37, -35);
+    this.line(-50, 45);
+    this.line(-5, -5);
+    this.line(50, -45);
+    ctx.closePath();
+    ctx.fillStyle = 'gray';
+    ctx.fill();
+    ctx.stroke();
+    //Wheel 1
+    ctx.beginPath();
+    this.move(-48, 47);
+    new Wheel(this.x, this.y, 60);
+    
+    //Connection to Hull 2
+    ctx.beginPath();
+    this.move(295, -52);
+    this.line(50, 45);
+    this.line(-5, 5);
+    this.line(-50, -45);
+    ctx.closePath();
+    ctx.fillStyle = 'gray';
+    ctx.fill();
+    ctx.stroke();
+  };
+  move(x, y){
+    this.x += x;
+    this.y += y;
+    ctx.moveTo(this.x, this.y);
+  };
+  line(x, y){
+    this.x += x;
+    this.y += y;
+    ctx.lineTo(this.x, this.y);
+  };
 }
 
-//Draw methods
-function drawHull(){
-  // Hull
-  ctx.beginPath();
-  move(middleX-100, middleY);
-  //Wheelcover(back)
-  line(175, 0);
-  line(0, -25);
-  line(75, -50);
-  line(-15, -5);
-  line(-75, 15);
-  //Tail
-  line(5, -20);
-  line(125, -50);
-  line(-125, -5);
-  line(-10, 25);
-  //Seat
-  line(-70, 5);
-  line(-30, -30);
-  line(-70, 5);
-  line(-25, 15);
-  //Front
-  line(-10, 0);
-  line(25, -50);
-  line(-20, 5);
-  line(-75, 100);
-  //Wheelcover(front)
-  line(50, 0);
-  line(70, 65);
-  ctx.closePath();
-
-  ctx.fillStyle = 'purple';
-  ctx.fill();
-  ctx.stroke();
-}
-
-function drawFrontWheel(){
-  //Connection to Hull
-  ctx.beginPath();
-  move(-37, -35);
-  line(-50, 45);
-  line(-5, -5);
-  line(50, -45);
-  ctx.closePath();
-  ctx.stroke();
-
-  //Wheel
-  ctx.beginPath();
-  move(-50, 45);
-  ctx.arc(curX, curY, 60, 0, 2*Math.PI);
-  ctx.stroke();
-
+class Wheel {
+  constructor(x, y, r){
+    this.x = x;
+    this.y = y;
+    //1
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, TWO_PI/6);
+    this.line(0, 0);
+    ctx.closePath();
+    ctx.stroke();
+    //2
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, (TWO_PI/6)*2);
+    this.line(0, 0);
+    ctx.closePath();
+    ctx.stroke();
+    //3
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, (TWO_PI/6)*3);
+    this.line(0, 0);
+    ctx.closePath();
+    ctx.stroke();
+    //4
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, (TWO_PI/6)*4);
+    this.line(0, 0);
+    ctx.closePath();
+    ctx.stroke();
+    //5
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, (TWO_PI/6)*5);
+    this.line(0, 0);
+    ctx.closePath();
+    ctx.stroke();
+    //6
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, TWO_PI);
+    this.line(0, 0);
+    ctx.closePath();
+    ctx.stroke();
+  }
+  line(x, y){
+    this.x += x;
+    this.y += y;
+    ctx.lineTo(this.x, this.y);
+  };
 }
 
 //Functional methods
-function move(x, y){
-  curX += x;
-  curY += y;
-  ctx.moveTo(curX, curY);
-}
-
-function line(x, y){
-  curX += x;
-  curY += y;
-  ctx.lineTo(curX, curY);
-}
-
-//Method calls
 initCanvas();
